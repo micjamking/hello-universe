ca0b04ebf84119b51ebc9bee02c169ea96d6afd4
Mike King <yo@mikeking.io>
2017-01-02 13:12:07 -1000

    Updated angular component list

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
