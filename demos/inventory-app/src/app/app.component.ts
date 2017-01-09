6553784d4868a75ab0187c4da3c5105c41f8dcf2
Mike King <yo@mikeking.io>
2017-01-08 16:43:07 -1000

    Adding inventory app product model and initial component annotations

--- demos/inventory-app/src/app/app.component.ts
+++ demos/inventory-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class InventoryApp {
+  product: Product[];
+
+  constructor (){
+    this.product = [
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      ),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      ),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      )
+    ];
+  }
 }
